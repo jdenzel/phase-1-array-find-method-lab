8b5c5f301caeef36b14634fc5fe4da51cc08d46a
Denzel <jdenzelmab@gmail.com>
2023-04-05 19:48:26 -0400

    Completed array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    let location = record.indexOf(record.find(r => r.result === 'W'));
+    if(record.find(r => r.result === 'W')){
+        return record[location].year;
+    }
+    else{
+        return undefined;
+    }
+}
